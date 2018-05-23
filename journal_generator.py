3898435243eab8f75a61f68d496c5fc6f9335f43
Marc Chesebro <Marc.chesebro@gmail.com>
2018-05-23 15:35:18 -0400

    Updated to generate word document

--- journal_generator.py
+++ journal_generator.py
@@ -1,8 +1,11 @@
 import datetime
 from dateutil import parser
 import re
+from docx import Document
+from docx.shared import Pt
 
-TAGS = ['bold', 'size']
+TAGS = ['bold', 'size', '/bold', '/size']
+
 
 def load_template():
     with open('_template.txt', 'r') as content_file:
@@ -48,33 +51,48 @@ def format_journal(dates, template):
 def parse_tags(template):
     lines = [re.split(r'[<|>]', line) for line in template.splitlines()]
     for line in lines:
-        print(line)
-
-
+        line.append('\n')
     return lines
 
 
-def docx_conversion(journal_lines):
+def docx_conversion(journal_lines, input_document):
     # not too sure how this should look
+    paragraph = input_document.add_paragraph()
+    _is_bold_ = False
+    _is_size_ = False
     for line in journal_lines:
         for e in line:
             if e in TAGS:
-                # add to doc with format stoping at ('/' + TAG)
-                pass
+                _is_bold_, _is_size_ = process_tag(e, _is_bold_, _is_size_)
             else:
-                # if not in tags, add to document unformated
-                pass
+                run = paragraph.add_run(e)
+                run.bold = _is_bold_
+                if _is_size_:
+                    font = run.font
+                    font.size = Pt(14)
+
+
+def process_tag(input_tag, bold, size):
+    if input_tag == TAGS[0]:
+        bold = True
+    elif input_tag == TAGS[1]:
+        size = True
+    elif input_tag == TAGS[2]:
+        bold = False
+    elif input_tag == TAGS[3]:
+        size = False
+    return bold, size
 
 
 def generate_journal(date):
     template = load_template()
     dates = create_date_list(date)
-    file_name = '{}-InternshipJournal-MarcChesebro.txt'.format(date.strftime("%m-%d-%Y"))
+    file_name = '{}-InternshipJournal-MarcChesebro.docx'.format(date.strftime("%m-%d-%Y"))
     journal_string = format_journal(dates, template)
     journal_lines = parse_tags(journal_string)
-    docx_conversion(journal_lines)
-
-    #write_file(file_name, format_journal(dates, template))
+    document = Document()
+    docx_conversion(journal_lines, document)
+    document.save(file_name)
 
 
 if __name__ == '__main__':
